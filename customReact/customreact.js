70ec345b9f554053b82a616965bf884acd40776b
Amit Yadav <amyd@gmail.com>
2025-05-31 13:24:12 +0530

    CustomReact app

---
+++ customReact/customreact.js
@@ -0,0 +1,19 @@
+function customeRender(reactElement, container) {
+    const domElement =document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    domElement.setAttribute('href', reactElement.props.href);
+    domElement.setAttribute('target', reactElement.props.target);
+    container.appendChild(domElement);
+    console.log(domElement);
+}
+const reactElement = {
+    type: 'a',
+    props: {
+        href : 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+const mainContainer = document.getElementById('root')
+customeRender(reactElement, mainContainer)
