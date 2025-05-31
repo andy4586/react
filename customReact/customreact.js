fca614c1263b67df76a33884ba4714a06ec59c74
Amit Yadav <amyd@gmail.com>
2025-05-31 13:36:55 +0530

    error solving fixing

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,14 +1,22 @@
 function customeRender(reactElement, container) {
-    const domElement =document.createElement(reactElement.type);
-    domElement.innerHTML = reactElement.children;
-    domElement.setAttribute('href', reactElement.props.href);
-    domElement.setAttribute('target', reactElement.props.target);
+    const domElement = document.createElement(reactElement.type);
+    if (typeof reactElement.children === 'string') {
+        domElement.innerText = reactElement.children;
+    }
+    if (reactElement.props) {
+        if (reactElement.props.href) {
+            domElement.setAttribute('href', reactElement.props.href);
+        }
+        if (reactElement.props.target) {
+            domElement.setAttribute('target', reactElement.props.target);
+        }
+    }
     container.appendChild(domElement);
 }
 const reactElement = {
     type: 'a',
     props: {
-        href : 'https://google.com',
+        href: 'https://google.com',
         target: '_blank'
     },
     children: 'Click me to visit Google'
